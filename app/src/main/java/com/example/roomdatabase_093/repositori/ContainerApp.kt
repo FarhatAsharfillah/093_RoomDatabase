6d8b3a8a719c3ac48a521f61c6f838ce6ce4bcdb
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-11-27 15:43:57 +0700

    menambahkan kode di containerapp dengan interface dan class

---
+++ app/src/main/java/com/example/roomdatabase_093/repositori/ContainerApp.kt
@@ -0,0 +1,18 @@
+package com.example.roomdatabase_093.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.roomdatabase_093.room.DatabaseSiswa
+
+interface ContainerApp{
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp( val context: Context):
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
+
