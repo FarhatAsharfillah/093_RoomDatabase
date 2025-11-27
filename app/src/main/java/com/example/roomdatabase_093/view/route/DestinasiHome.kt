f6ed6bc29b9f67108ea701e0a3b202c62b8b3e70
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-11-27 15:51:47 +0700

    menambahkan kode object destinasihome dan override

---
+++ app/src/main/java/com/example/roomdatabase_093/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.roomdatabase_093.view.route
+
+import com.example.roomdatabase_093.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
