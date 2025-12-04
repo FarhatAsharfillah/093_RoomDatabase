f2f8aba3e371de7267d8be7c69bab93c07632e58
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-12-04 15:22:28 +0700

    membuat ViewModel untuk EditViewModel

---
+++ app/src/main/java/com/example/roomdatabase_093/viewmodel/EditViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.roomdatabase_093.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomdatabase_093.repositori.RepositoriSiswa
+import com.example.roomdatabase_093.view.route.DestinasiDetailSiswa
+
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetailSiswa.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoriSiswa.getSiswaStream(idSiswa)
+                .filterNotNull()
+                .first()
+                .toUiStateSiswa(true)
+        }
+    }
+
